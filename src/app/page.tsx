96a6b5eb40d56a84015dd26b7208bd89b4dc040a
Burak Sürümcüoğlu <bsurumcuoglu@gmail.com>
2023-06-23 16:15:30 +0300

    added loading.tsx that works with suspense under the hood

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <div>Home Page</div>
+      <a href="http://localhost:3000/try">Go to TRY</a>
+    </>
+  );
 }
